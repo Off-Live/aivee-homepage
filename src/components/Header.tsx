591cee28df484fd0dbb73193d96f9c2c08bc4b41
brian <brian@local.host>
2024-07-24 18:18:03 +0700

    smaller logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 const Header: React.FC = () => {
   return (
-    <nav className="flex justify-center items-center px-16 py-10 w-full font-semibold bg-zinc-950 max-md:px-5 max-md:max-w-full sticky top-0 z-50">
+    <nav className="flex justify-center items-center px-16 py-5 w-full font-semibold bg-zinc-950 max-md:px-5 max-md:max-w-full sticky top-0 z-50">
       <div className="flex gap-0 justify-between max-md:flex-wrap w-full max-w-[1440px] max-md:max-w-full">
         <div className="text-4xl text-white whitespace-nowrap max-md:flex-wrap">
           <Link className="flex flex-1 gap-2.5" to={'/'}>
@@ -14,7 +14,7 @@ const Header: React.FC = () => {
               loading="lazy"
               src="/aivee.png"
               alt=""
-              className="shrink-0 aspect-square w-[87px]"
+              className="shrink-0 aspect-square w-[50px]"
             />
             <div className="my-auto text-xl max-md:max-w-full max-sm:invisible">AiVee</div>
           </Link>
